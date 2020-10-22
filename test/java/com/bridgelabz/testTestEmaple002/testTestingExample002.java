6c9045942a14c7be7978de87a6ae4626dc47848d
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 12:37:26 +0530

    Added : code for testing example

---
+++ test/java/com/bridgelabz/testTestEmaple002/testTestingExample002.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.testTestEmaple002;
+
+import org.junit.Test;
+
+public class testTestingExample002 {
+	@Test
+	public static void main(String[] args) {
+		System.out.println(" Hello everyone ");
+	}
+
+}
